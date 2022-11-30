db4715505f220b5f6a5904372590e7a05dd27436
Migeuskz <msanchezpapalotzi@gmail.com>
2022-11-30 12:01:53 -0600

    Aver si ahora si

---
+++ js/app.js
@@ -0,0 +1,10 @@
+import { showModal, navegacion, loadData, urlBase } from './eventos.js';
+
+loadData(urlBase);
+document.querySelector('#respuesta').addEventListener('click', showModal);
+document.querySelector('.botones').addEventListener('click', navegacion);
+
+
+
+
+
